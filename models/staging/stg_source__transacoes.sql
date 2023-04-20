7bb57319a0a06703b8cb6d5e639af2b4e482f808
gustavoCorreiaGonzalez <gustavo.correia.gonzalez@gmail.com>
2023-04-19 22:40:33 -0300

    refactor: Melhorando o modelo de staging
    Foi necessário realizar algumas formatações nos dados para que os modelos intermediários e dimensões não precisassem realizar transformações nos dados

---
+++ models/staging/stg_source__transacoes.sql
@@ -0,0 +1,26 @@
+-- stg_source__transacoes.sql
+
+WITH source AS (
+    SELECT * FROM {{ source('dev','raw_transacoes_usuarios') }}
+),
+
+transacoes AS (
+    SELECT
+        -- ids
+        codigo_da_transacao AS id_codigo_transacao,
+        codigo_do_usuario AS id_codigo_usuario,
+        -- strings
+        LOWER(metodo_de_captura) AS nm_metodo_captura,
+        LOWER(bandeira_do_cartao) AS nm_bandeira_cartao,
+        LOWER(metodo_de_pagamento) AS nm_metodo_pagamento,
+        LOWER(estado_da_transacao) AS nm_estado_transacao,
+        LOWER(estado_do_usuario) AS nm_estado_usuario,
+        LOWER(cidade_do_usuario) AS nm_cidade_usuario,
+        -- numerics
+        CAST(valor_da_transacao AS FLOAT) AS nm_valor_transacao,
+        -- dates
+        TO_TIMESTAMP(data_e_hora_da_transacao, 'YYYY-MM-DD HH24:MI:SS.MS') AS dt_transacao
+    FROM source
+)
+
+SELECT * FROM transacoes
