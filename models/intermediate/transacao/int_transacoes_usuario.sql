b725466cf8966313c46d347acf40c1e25d81e9f7
gustavoCorreiaGonzalez <gustavo.correia.gonzalez@gmail.com>
2023-04-24 18:57:19 -0300

    fix: :bug: Removendo coluna irrelevante

--- models/intermediate/transacao/int_transacoes_usuario.sql
+++ models/intermediate/transacao/int_transacoes_usuario.sql
@@ -1,18 +1,19 @@
--- int_churn_transacoes_usuario.sql
+-- int_transacoes_usuario.sql
 
 WITH stg_stone__transacoes AS (
-  SELECT * FROM {{ ref('stg_stone__transacoes') }}
+    SELECT * FROM {{ ref('stg_stone__transacoes') }}
 ),
 
 transacoes_realizadas AS (
     SELECT
         id_codigo_usuario,
-        nm_estado_transacao,
         nm_metodo_pagamento,
         nm_valor_transacao,
         dt_transacao
-    FROM stg_stone__transacoes
-    WHERE nm_estado_transacao = 'paid'
+    FROM
+        stg_stone__transacoes
+    WHERE
+        nm_estado_transacao = 'paid'
 )
 
 SELECT * FROM transacoes_realizadas
